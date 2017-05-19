c95ad1d83a2bc9a54a5c594a853be4ced5bc0aa7
DoctorLynn <252279756@qq.com>
2017-05-19 15:05:53 +0800

    日常修改

--- utils/jsonUtil.js
+++ utils/jsonUtil.js
@@ -22,6 +22,7 @@ jsonUtil.getJsonFileData = function(u){
 	    alert("请升级至最新版本的浏览器");  
 	}  
 	if(xhr !=null){  
+		alert(u);
 		alert(jsonUtil.getFileExt(u));
 		if(u && jsonUtil.getFileExt(u) == "json"){
 			xhr.open("GET", u, true);  
