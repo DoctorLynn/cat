bb2fc9d29e4f2579960062b94b8732712cc10e7c
DoctorLynn <252279756@qq.com>
2017-05-19 15:19:24 +0800

    日常修改

--- utils/jsonUtil.js
+++ utils/jsonUtil.js
@@ -25,11 +25,14 @@ jsonUtil.getJsonFileData = function(u){
 		alert(u);
 		alert(jsonUtil.getFileExt(u));
 		if(u && jsonUtil.getFileExt(u) == "json"){
-			xhr.open("GET", "../../config/index/indexConf.json", true);  
+			xhr.open("GET", "config/index/indexConf.json", true);  
 		    xhr.send(null);  
 		    xhr.onreadystatechange=function(){  
 		        if(xhr.readyState == 4 && xhr.status == 200){  
-		            r = JSON.parse(xhr.responseText);  
+		            alert(xhr);
+		            alert(xhr.responseText);
+		            r = JSON.parse(xhr.responseText);
+		            alert(r);
 		        }  
 	   	 	};  
 		}else{
