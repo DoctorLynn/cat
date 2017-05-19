70d076b84b4bf6eb77029f537ed3dbe786bbfee6
DoctorLynn <252279756@qq.com>
2017-05-19 17:39:42 +0800

    日常修改

--- js/index/index_main.js
+++ js/index/index_main.js
@@ -1,23 +1,26 @@
 var indexMain = {
 	isAdmin: false,
-	isOk: false
+	isOk: false,
+	interval: null
 };
 
 function checkUserLogin(d, u, p){
 	var isAdmin = indexMain.isAdmin;
 	if(isAdmin){
+		clearInterval(indexMain.interval);
 		var a = d.admin;
 		if(a){
+			jsUtil.maskTopHide();
 			if(u == a.username && p == a.password){
-				jsUtil.maskTopHide();
 				window.location.href = "pages/theme/theme_main.html";
 			}else{
-				alert("用户名或密码输入错误!!!");
+				alert("用户名或密码输入错误!!!"); return false;
 			}
 		}
 	}else{
 		var v = d.visitor, l = v.length, i = 0;
 		if(l > 0){
+			jsUtil.maskTopHide();
 			for (; i < l; i++) {
 				if(u == v[i].username && p == v[i].password){
 					indexMain.isOk = true;
@@ -25,10 +28,9 @@ function checkUserLogin(d, u, p){
 				}
 			}
 			if(indexMain.isOk){
-				jsUtil.maskTopHide();
 				window.location.href = "pages/theme/theme_main.html";
 			}else{
-				alert("用户名或密码输入错误!!!");
+				alert("用户名或密码输入错误!!!"); return false;
 			}
 		}
 	}
@@ -48,16 +50,16 @@ function loginSubmit(){
 				alert("ERROR: DATA OBJECT IS NULL!");
 			}
 		});
-		var s = setInterval(function(){
+		indexMain.interval = setInterval(function(){
 			if(t && (new Date().getTime() - t) > 10000){
 				jsUtil.maskTopHide();
-				clearInterval(s);
+				clearInterval(indexMain.interval);
 				alert("请求超时,请稍候再试!");
 				return false;
 			}
 		}, 1000);
 	}else{
-		alert("请输入用户名和密码!");
+		alert("请输入用户名和密码!"); return false;
 	}
 }
 
